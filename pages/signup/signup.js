3acd2b8cfb88dc9d4ac6ef75ea7a58220bf32a83
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-11-21 09:35:22 +0100

    Adds signup functionality and error handling

--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -11,11 +11,23 @@ export function initSignup(){
 async function signup(){
     event.preventDefault();
     
+    document.querySelector('#invalid-feedback1').innerHTML = "";
+    document.querySelector('#invalid-feedback2').innerHTML = "";
+    document.querySelector('#invalid-feedback3').innerHTML = "";
+    document.querySelector('#invalid-feedback4').innerHTML = "";
+    document.querySelector('#invalid-feedback5').innerHTML = "";
+    document.querySelector('#invalid-feedback6').innerHTML = "";
+
     const firstName = document.querySelector("#input-firstname").value;
     const lastName = document.querySelector("#input-lastname").value;
     const email = document.querySelector("#input-email").value;
     const username = document.querySelector("#input-username").value;
     const password = document.querySelector("#input-password").value;
+    const passwordCheck = document.querySelector("#input-password2").value;
+
+    if(!isSignupValid(firstName, lastName, email, username, password, passwordCheck)){
+        return;
+    }
 
     const signupRequest = {
         firstName: firstName,
@@ -29,7 +41,48 @@ async function signup(){
         const res = await fetch(URL, makeOptions("POST", signupRequest)).then(r => handleHttpErrors(r))
         console.log(res)
     } catch(err){
-        console.log(err.message)
+        if(err.message === "User already exists"){
+            document.querySelector('#invalid-feedback3').innerHTML = "Brugernavn er allerede i brug."
+        }
+        if(err.message === "Email already exists"){
+            document.querySelector('#invalid-feedback4').innerHTML = "Email er allerede i brug."
+        }
     }
 
 }
+
+function isSignupValid(firstName, lastName, email, username, password, passwordCheck){
+    if(!firstName || firstName.trim() === ""){
+        document.querySelector('#invalid-feedback1').innerHTML = "Indtast venligst fornavn."
+        return false;
+    }
+
+    if(!lastName || lastName.trim() === ""){
+        document.querySelector('#invalid-feedback2').innerHTML = "Indtast venligst efternavn."
+        return false;
+    }
+
+    if(!username || username.trim() === "" || username.length < 2){
+        document.querySelector('#invalid-feedback3').innerHTML = "Indtast venligst brugernavn."
+        return false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!email || email.trim() === "" || !emailRegex.test(email)){
+        document.querySelector('#invalid-feedback4').innerHTML = "Indtast venlist en gyldig emailadresse."
+        return false;        
+    }
+    
+    if(!password || password.length < 4){
+        document.querySelector('#invalid-feedback5').innerHTML = "Kodeord skal være mindst 4 tegn."
+        return false;
+    }
+    
+    if(password !== passwordCheck){
+        document.querySelector('#invalid-feedback6').innerHTML = "Kodeord er ikke ens."
+        return false;
+    }
+
+    return true;
+}
+
