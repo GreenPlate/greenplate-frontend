f694c3efb2c9c77d62cc60cf8affe040a34e528b
steffengroenandersen <steffengroenandersen@gmail.com>
2023-11-28 09:58:54 +0100

    Add recipe get mapping

--- pages/recipes-overview/recipes-overview.js
+++ pages/recipes-overview/recipes-overview.js
@@ -1,3 +1,5 @@
+import { API_URL } from "./../../settings.js"
+
 import { handleHttpErrors, makeOptions, sanitizeStringWithTableRows } from "./../../utility.js"
 
 export async function initRecipesOverview(){
@@ -11,68 +13,8 @@ async function getRecipes(){
     console.log("getRecipes()")
     
     // fetch recipes from database
-    const recipes = [
-        {
-            "recipeId": 1,
-            "recipeTitle": "Spaghetti Bolognese",
-            "recipeBody": "Classic Italian pasta dish with a rich meat sauce.",
-            "recipeIngredients": "500g ground beef, 1 onion, 2 cloves garlic, 1 can crushed tomatoes, 1 tsp oregano, 2 tbsp tomato paste, salt, pepper, 400g spaghetti"
-        },
-        {
-            "recipeId": 2,
-            "recipeTitle": "Chicken Caesar Salad",
-            "recipeBody": "A refreshing salad with grilled chicken and Caesar dressing.",
-            "recipeIngredients": "2 boneless chicken breasts, Romaine lettuce, croutons, Parmesan cheese, Caesar dressing"
-        },
-        {
-            "recipeId": 3,
-            "recipeTitle": "Vegetarian Stir Fry",
-            "recipeBody": "Quick and healthy stir-fried vegetables with tofu.",
-            "recipeIngredients": "1 block tofu, broccoli, bell peppers, carrots, soy sauce, ginger, garlic, sesame oil"
-        },
-        {
-            "recipeId": 4,
-            "recipeTitle": "Mango Avocado Salsa",
-            "recipeBody": "A fruity salsa with a hint of spice.",
-            "recipeIngredients": "2 ripe mangoes, 2 avocados, red onion, cilantro, lime juice, jalapeño, salt"
-        },
-        {
-            "recipeId": 5,
-            "recipeTitle": "Shrimp Scampi Pasta",
-            "recipeBody": "Garlicky shrimp served over a bed of linguine.",
-            "recipeIngredients": "250g linguine, 1 lb shrimp, garlic, white wine, lemon juice, parsley, salt, pepper"
-        },
-        {
-            "recipeId": 6,
-            "recipeTitle": "Caprese Salad",
-            "recipeBody": "Simple and classic Italian salad with fresh tomatoes, mozzarella, and basil.",
-            "recipeIngredients": "Tomatoes, fresh mozzarella, fresh basil, balsamic glaze, olive oil, salt, pepper"
-        },
-        {
-            "recipeId": 7,
-            "recipeTitle": "BBQ Chicken Pizza",
-            "recipeBody": "Homemade pizza topped with BBQ chicken, red onions, and cilantro.",
-            "recipeIngredients": "Pizza dough, cooked chicken, BBQ sauce, red onion, cilantro, mozzarella cheese"
-        },
-        {
-            "recipeId": 8,
-            "recipeTitle": "Quinoa Salad with Roasted Vegetables",
-            "recipeBody": "Healthy and hearty salad with quinoa, roasted veggies, and a lemon vinaigrette.",
-            "recipeIngredients": "Quinoa, cherry tomatoes, bell peppers, zucchini, red onion, olive oil, lemon juice, feta cheese"
-        },
-        {
-            "recipeId": 9,
-            "recipeTitle": "Beef and Broccoli Stir Fry",
-            "recipeBody": "Quick and flavorful stir fry with tender beef and crisp broccoli.",
-            "recipeIngredients": "500g beef sirloin, broccoli, soy sauce, ginger, garlic, sesame oil, rice vinegar"
-        },
-        {
-            "recipeId": 10,
-            "recipeTitle": "Chocolate Banana Smoothie",
-            "recipeBody": "A delicious and indulgent smoothie with chocolate and banana.",
-            "recipeIngredients": "2 ripe bananas, milk, chocolate protein powder, peanut butter, ice cubes"
-        },
-    ];
+    console.log(API_URL + "/recipes");
+    const recipes = await fetch(API_URL + "/recipes/admin", makeOptions("GET", null, true)).then(r => handleHttpErrors(r));
 
     // Map recipes to innerHTML
     const recipeRows = recipes.map(recipe => `
